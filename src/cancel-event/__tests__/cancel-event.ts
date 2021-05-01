3d3344f112728e692771f33ba1a65cfa43b3d4bd
Ben Heidemann <ben@heidemann.co.uk>
2021-05-01 16:13:04 +0100

    test: add tests for cancelEvent helper

---
+++ src/cancel-event/__tests__/cancel-event.ts
@@ -0,0 +1,53 @@
+import { cancelEvent } from "..";
+
+describe('cancelEvent', () => {
+    it('should call preventDefault and set cancelBubble to true when cnacelable is undefined', () => {
+        // Arrange
+        const event: Partial<Event> = {
+            cancelBubble: false,
+            preventDefault: jest.fn(),
+        };
+
+        // Act
+        const wasCancelled = cancelEvent(event as Event);
+
+        // Assert
+        expect(event.cancelBubble).toBeTruthy();
+        expect(event.preventDefault).toHaveBeenCalledTimes(1);
+        expect(wasCancelled).toBeTruthy();
+    });
+
+    it('should call preventDefault and set cancelBubble to true when cancelable is true', () => {
+        // Arrange
+        const event: Partial<Event> = {
+            cancelable: true,
+            cancelBubble: false,
+            preventDefault: jest.fn(),
+        };
+
+        // Act
+        const wasCancelled = cancelEvent(event as Event);
+
+        // Assert
+        expect(event.cancelBubble).toBeTruthy();
+        expect(event.preventDefault).toHaveBeenCalledTimes(1);
+        expect(wasCancelled).toBeTruthy();
+    });
+
+    it('should not call preventDefault or set cancelBubble to true when cancelable is false', () => {
+        // Arrange
+        const event: Partial<Event> = {
+            cancelable: false,
+            cancelBubble: false,
+            preventDefault: jest.fn(),
+        };
+
+        // Act
+        const wasCancelled = cancelEvent(event as Event);
+
+        // Assert
+        expect(event.cancelBubble).toBeFalsy();
+        expect(event.preventDefault).not.toHaveBeenCalled();
+        expect(wasCancelled).toBeFalsy();
+    });
+});
