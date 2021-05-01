eac8dc46225bcd0afe9af7aa3696a7a98e6ab8ef
Ben Heidemann <ben@heidemann.co.uk>
2021-05-01 16:12:35 +0100

    feat: add cancelEvent helper

---
+++ src/cancel-event/index.ts
@@ -0,0 +1,13 @@
+/**
+ * This function accepts an event and attempts to cancel it.
+ * @param event {Event} - The event to be cancelled
+ * @returns {boolean} true if the event was cancelled or false if it couldn't be cancelled
+ */
+ export function cancelEvent(event: Event) {
+    if (typeof event.cancelable !== 'boolean' || event.cancelable) {
+        event.cancelBubble = true;
+        event.preventDefault();
+        return true;
+    }
+    return false;
+}
