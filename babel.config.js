0a27dda77e9404fed28a5630bb9e60ad256ab67d
Ben Heidemann <ben@heidemann.co.uk>
2021-05-01 16:01:02 +0100

    test: setup tests with ts-jest

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,8 @@
 module.exports = {
-    presets: [
-        ['@babel/preset-env', { targets: { node: 'current' } }],
-        '@babel/preset-typescript',
-    ],
+    "presets": ["@babel/preset-env"],
+    "plugins": [
+        ["@babel/plugin-transform-runtime", { "regenerator": true }],
+        ["@babel/plugin-proposal-decorators", { "legacy": true }],
+        ["@babel/plugin-proposal-class-properties", { "loose": false }]
+    ]
 };
