ed400ed77527f6769811519ca95410f3248e6ed2
Ben Heidemann <ben@heidemann.co.uk>
2021-05-02 05:07:02 +0100

    refactor: update cancelEvent code

--- src/cancel-event/index.ts
+++ src/cancel-event/index.ts
@@ -1,13 +1,23 @@
+function isEventCancellable(event: Event) {
+    return typeof event.cancelable !== 'boolean' || event.cancelable;
+}
+
+function cancelEventInternal(event: Event) {
+    event.cancelBubble = true;
+    event.preventDefault();
+}
+
 /**
  * This function accepts an event and attempts to cancel it.
  * @param event {Event} - The event to be cancelled
  * @returns {boolean} true if the event was cancelled or false if it couldn't be cancelled
  */
  export function cancelEvent(event: Event) {
-    if (typeof event.cancelable !== 'boolean' || event.cancelable) {
-        event.cancelBubble = true;
-        event.preventDefault();
+    if (isEventCancellable(event)) {
+        cancelEventInternal(event);
         return true;
     }
-    return false;
+    else {
+        return false;
+    }
 }
